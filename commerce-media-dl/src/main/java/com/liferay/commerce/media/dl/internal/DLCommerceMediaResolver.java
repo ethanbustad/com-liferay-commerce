b6d110076b5ba6e408ed014bb87a9fc810ae2415
Alec Sloan <Alec.sloan@Liferay.com>
2019-01-17 11:06:28 -0800

    COMMERCE-553 Remove cpDefinitionId from params and implement permission check

--- commerce-media-dl/src/main/java/com/liferay/commerce/media/dl/internal/DLCommerceMediaResolver.java
+++ commerce-media-dl/src/main/java/com/liferay/commerce/media/dl/internal/DLCommerceMediaResolver.java
@@ -53,7 +53,6 @@ import java.io.IOException;
 
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
